884c46c462df385193829a3bd3f7300b58d1386f
LaNeitD18 <phanhuytien14@gmail.com>
2021-03-15 21:41:53 +0700

    Finish Singleton

---
+++ Singleton/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SoundManager soundManager = SoundManager.getInstance();
+            SoundManager temp = SoundManager.getInstance();
+
+            soundManager.addSound("AAA");
+            temp.addSound("ZZZ");
+
+            soundManager.playSound(1);
+        }
+    }
+}
---
+++ Singleton/SoundManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    public class SoundManager
+    {
+        private static SoundManager _instance;
+        private List<string> listSound = new List<string>();
+
+        private SoundManager() { }
+
+        public static SoundManager getInstance()
+        {
+            // lazy init
+            if(_instance == null) {
+                _instance = new SoundManager();
+            }
+            return _instance;
+        }
+
+        public void addSound(string soundName) {
+            listSound.Add(soundName);
+            Console.WriteLine("Added sound successfully!");
+        }
+
+        public void playSound(int index) { 
+            if(index < listSound.Count) {
+                Console.WriteLine("Playing sound: " + listSound[index]);
+            } else {
+                Console.WriteLine("Sound invalid");
+            }
+        }
+
+        public int getNumberOfSounds() {
+            return this.listSound.Count;
+        }
+    }
+}
